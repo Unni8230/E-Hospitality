b76498d8ad6e4832ca5ad00f9bcc261df192c7b2
Unnikrishnan <unnioffi0620@gmail.com>
2025-06-03 17:10:01 +0530

    Added Login and Register user functionality, Created Credentials table for storing user details

--- Hospital/HospitalManagement/models.py
+++ Hospital/HospitalManagement/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Credentials(models.Model):
+    username = models.CharField(max_length=100,unique=True, default="Unknown")
+    name = models.CharField(max_length=255, default="Unknown")
+    email = models.EmailField(max_length=255,unique=True, default="Unknown")
+    phone = models.CharField(max_length=12,default="Unknown")
+    password = models.CharField(max_length=255, default="Unknown")
+    type=models.CharField(max_length=50,default="Unknown")
+    def __str__(self):
+        return self.username
+
+class Doctor(models.Model):
+    doctor_name = models.CharField(max_length=100,default="None")
+    specialization = models.CharField(max_length=100,default="None")
+    def __str__(self):
+        return self.doctor_name
+class Appointments(models.Model):
+    user = models.ForeignKey(Credentials, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()  
+    doctors_description = models.CharField(max_length=255,default="") 
+    medicines = models.CharField(max_length=255,default="None") 
+
+class PatientRecords(models.Model):
+    username = models.ForeignKey(Credentials,on_delete=models.CASCADE)
+    disease = models.CharField(max_length=255,default="None")
+    
+    
--- Hospital/HospitalManagement/views.py
+++ Hospital/HospitalManagement/views.py
@@ -1,4 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import urls
+import bcrypt
+from .models import *
+from django.contrib import messages
+# Create your views here.
 
 def homePage(request):
     return render(request, 'homePage.html')
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = Credentials.objects.filter(username=username).first()
+        if user is not None:
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                if user.type == "Patient":
+                    request.session['user_id'] = user.id
+                    return redirect('userprofile')
+                elif user.type == "Doctor":
+                    request.session['user_id'] = user.id
+                    return redirect('doctorprofile')
+                elif user.type=="Admin":
+                    request.session['user_id'] = user.id
+                    return redirect('adminprofile')
+            else:
+                messages.error(request,"Invalid Password!")
+                return redirect('login')
+        else:
+            messages.error(request,"Invalid User!")
+            return redirect('login')
+    return render(request,'loginPage.html')
+def logout(request):
+    request.session.flush()
+    return redirect('login')
+def registerUser(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        username = request.POST['username']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        type = request.POST['type']
+
+        if(password != cpassword):
+            messages.error(request,"Passwords do not match!")
+            return redirect('register')
+        else:
+            if Credentials.objects.filter(email=email).exists():
+                messages.error(request,"Username already taken")
+                return redirect('register')
+            elif Credentials.objects.filter(email=email).exists():
+                messages.error(request,"Email already taken")
+                return redirect('register')
+            else:
+                hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+                Credentials.objects.create(name=name,email=email,phone=phone,username=username,password=hashed_password,type=type)
+                messages.success(request,"Registration Succesful, Please Login")
+                return redirect('login')
+    return render(request,'registerUser.html')
